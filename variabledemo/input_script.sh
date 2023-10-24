cb96e80c0a919f4513616c44ea57331361e9b566
Jayaram318 <jayaramireddy318@gmail.com>
2023-10-24 10:59:27 +0530

    change3

--- variabledemo/input_script.sh
+++ variabledemo/input_script.sh
@@ -7,6 +7,7 @@
 #description   : useing the input from command line
 #############################################################################################
 # we give name value at command line filename.sh=$0 <name>=$1
+# here $1 is positional argument
 name=${1}
 echo "hello $name"
 echo "0=${0} 1=${1}"
