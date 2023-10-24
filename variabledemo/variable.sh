4a7b25f4bec3828170d76885de163e4c998a7fd8
Jayaram318 <jayaramireddy318@gmail.com>
2023-10-24 10:26:04 +0530

    variable usage

---
+++ variabledemo/variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#############################################################################################
+#author        : K Jayaramireddy
+#date          : 24-10-2023
+#usage         : ./variable.sh
+#description   : useing the variables
+#############################################################################################
+
+Name=K.Jayaramreddy
+state_name=Andhra_pradesh
+
+echo "This is $Name,from $state_name"
+number=4
+echo $number+2
