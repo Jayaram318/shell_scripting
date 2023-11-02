be6710560baefad8dc6bd25db4bdc51a29f4dacf
Jayaram318 <jayaramireddy318@gmail.com>
2023-11-02 22:47:02 +0530

    variable scope

---
+++ variabledemo/script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name=bash_scripting
+
+echo "this is from script1 $name"
+
+./script2.sh
---
+++ variabledemo/script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echogit "this is from script2"
+
+echo"value of variable $name"
