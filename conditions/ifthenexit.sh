dbdfa96230434e250ae0b47e45459dd8428f8a27
Jayaram318 <jayaramireddy318@gmail.com>
2023-11-13 11:00:37 +0530

    if condition

---
+++ conditions/ifthenexit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# check the file exit or does not using if condition
+
+FILE=/tmp/new1.txt
+
+if [[ -f ${FILE} ]]; then
+
+   cat ${FILE}
+
+   exit 0
+fi
+
+exit 1
+
+OUTPUT:
+
+ubuntu@ip-172-31-29-182:/$ sudo vi ifthen.sh
+ubuntu@ip-172-31-29-182:/$ sudo chmod +x ifthen.sh
+ubuntu@ip-172-31-29-182:/$ ./ifthen.sh
+./ifthen.sh: line 12: syntax error near unexpected token `fi'
+./ifthen.sh: line 12: `fi'
+ubuntu@ip-172-31-29-182:/$ sudo vi ifthen.sh
+ubuntu@ip-172-31-29-182:/$ ./ifthen.sh
+./ifthen.sh: line 12: syntax error near unexpected token `fi'
+./ifthen.sh: line 12: `fi'
+ubuntu@ip-172-31-29-182:/$ sudo vi ifthen.sh
+ubuntu@ip-172-31-29-182:/$ ./ifthen.sh
+ubuntu@ip-172-31-29-182:/$ echo $?
+1
+ubuntu@ip-172-31-29-182:/$ sudo touch /tmp/new1.txt
+ubuntu@ip-172-31-29-182:/$ ./ifthen.sh
+ubuntu@ip-172-31-29-182:/$ echo $?
+0
+ubuntu@ip-172-31-29-182:/$ sudo vi /tmp/new1.txt
+ubuntu@ip-172-31-29-182:/$ ./ifthen.sh
+hi this is Jayaram
+
+this is first script in conditions
+ubuntu@ip-172-31-29-182:/$ bash -x ./ifthen.sh
++ FILE=/tmp/new1.txt
++ [[ -f /tmp/new1.txt ]]
++ cat /tmp/new1.txt
+hi this is Jayaram
+
+this is first script in conditions
++ exit 0
+ubuntu@ip-172-31-29-182:/$
